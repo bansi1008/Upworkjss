680433c3096781a7791c56df08039ff5f2d6d2ab
bansi <bansidobariya2510@gmail.com>
2024-08-24 20:15:51 +0530

    Add Hello.js component

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-export default function Hello() {
-  return (
-    <div>
-      <p>Hello</p>
-    </div>
-  )
+function Hello() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
 }
+
+export default Hello;
+ 
